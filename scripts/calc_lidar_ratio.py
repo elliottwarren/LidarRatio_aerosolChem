58e32e96418016f4b143e801004e30530029178c
elliott <e.l.warren@pgr.reading.ac.uk>
2017-11-17 15:44:52 +0000

    Init commit

---
+++ scripts/calc_lidar_ratio.py
@@ -0,0 +1,51 @@
+"""
+Read in mass data from London and calculate the Lidar ratio using the CLASSIC scheme approach, from Ben's help.
+
+Created by Elliott Fri 17 Nov 2017
+"""
+
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+import pickle
+
+import numpy as np
+import datetime as dt
+
+import ellUtils as eu
+from forward_operator import FOUtils as FO
+from forward_operator import FOconstants as FOcon
+
+def main():
+
+
+    # Read in the mass data for 2016
+    # Read in RH data for 2016
+    # convert gases and such into the aerosol particles
+    # swell the particles based on the CLASSIC scheme stuff
+    # use Mie code to calculate the backscatter and extinction
+    # calculate lidar ratio
+    # plot lidar ratio
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return
+
+if __name__ == '__main__':
+    main()
+
+print 'END PROGRAM'
