643ef38f9e6e41a4c52cf911a7692bde5f83baf0
elliott <e.l.warren@pgr.reading.ac.uk>
2017-11-22 14:39:49 +0000

    Much more organised with functions seperated from the main body

--- scripts/calc_lidar_ratio.py
+++ scripts/calc_lidar_ratio.py
@@ -15,6 +15,86 @@ import ellUtils as eu
 from forward_operator import FOUtils as FO
 from forward_operator import FOconstants as FOcon
 
+# Read
+def read_mass_data(massdatadir, year):
+
+    """
+    Read in the mass data from NK
+    Raw data is micrograms m-3 but converted to and outputed as grams m-3
+    :param year:
+    :return: mass
+    """
+
+    massfname = 'PM_North_Kensington_'+year+'.csv'
+    massfilepath = massdatadir + massfname
+    massrawData = np.genfromtxt(massfilepath, delimiter=',', dtype="|S20") # includes the header
+
+    mass = {'time': np.array([dt.datetime.strptime(i[0], '%d/%m/%Y %H:%M') for i in massrawData[1:]])}
+
+    # get headers without the site part of it (S04@NK to S04)
+    headers = [i.split('@')[0] for i in massrawData[0][1:]]
+
+    # ignore first entry, as that is the date&time
+    for h, header_site in enumerate(massrawData[0][1:]):
+
+        # get the main part of the header from the
+        split = header_site.split('@')
+        header = split[0]
+
+        # turn '' into nans
+        # convert from micrograms to grams
+        mass[header] = np.array([np.nan if i[h+1] == '' else i[h+1] for i in massrawData[1:]], dtype=float) * 1e-06
+
+
+    # QAQC - turn all negative values in each column into nans if one of them is negative
+    for header_i in headers:
+        idx = np.where(mass[header_i] < 0.0)
+        for header_j in headers:
+            mass[header_j][idx] = np.nan
+
+    return mass
+
+def trim_mass_wxt_times(mass, WXT):
+
+    """
+    Trim the mass and WXT data based on their start and end times
+    DOES NOT CHECK INTERNAL TIME MATCHING (BULK TRIM APPROACH)
+    :return: mass
+    :return: wxt
+    """
+
+    # Find start and end idx for mass and WXT
+    if WXT['time'][0] < mass['time'][0]:
+        wxt_start = np.where(WXT['time'] == mass['time'][0])[0][0]
+        mass_start = 0
+    else:
+        wxt_start = 0
+        mass_start = np.where(mass['time'] == WXT['time'][0])[0][0]
+    # END
+    if WXT['time'][-1] > mass['time'][-1]:
+        wxt_end = np.where(WXT['time'] == mass['time'][-1])[0][0]
+        mass_end = len(mass['time']) - 1
+    else:
+        wxt_end = len(WXT['time']) - 1
+        mass_end = np.where(mass['time'] == WXT['time'][-1])[0][0]
+
+    # create idx ranges where data exists for both mass and WXT
+    wxt_range = np.arange(wxt_start, wxt_end, 1)
+    mass_range = np.arange(mass_start, mass_end, 1)
+
+    # trim data by only selecting the right time ranges
+    for key, data in WXT.iteritems():
+        WXT[key] = data[wxt_range]
+
+    for key, data in mass.iteritems():
+        mass[key] = data[mass_range]
+
+    # returned data should have the same start and end times
+
+    return mass, WXT
+
+# Process
+
 def calc_amm_sulph_and_amm_nit_from_gases(moles, mass):
 
     """
@@ -23,54 +103,118 @@ def calc_amm_sulph_and_amm_nit_from_gases(moles, mass):
 
     :param moles:
     :param mass:
-    :return:
+    :return: mass [with the extra entries for the particles]
     """
+
+    # define aerosols to make
+    mass['(NH4)2SO4'] = np.empty(len(moles['SO4']))
+    mass['(NH4)2SO4'][:] = np.nan
+    mass['NH4NO3'] = np.empty(len(moles['SO4']))
+    mass['NH4NO3'][:] = np.nan
 
     # calculate moles of the aerosols
     # help on GCSE bitesize:
     #       http://www.bbc.co.uk/schools/gcsebitesize/science/add_gateway_pre_2011/chemical/reactingmassesrev4.shtml
-    if moles['SO4'] > (moles['NH4'] / 2):  # more SO4 than NH4 (2 moles NH4 to 1 mole SO4) # needs to be divide here not times
+    for i in range(len(moles['SO4'])):
+        if moles['SO4'][i] > (moles['NH4'][i] / 2):  # more SO4 than NH4 (2 moles NH4 to 1 mole SO4) # needs to be divide here not times
 
-        # all of the NH4 gets used up making amm sulph.
-        mass['(NH4)2SO4'] = mass['NH4'] * 7.3  # ratio of molecular weights between amm sulp and nh4
-        # rem_nh4 = 0
-
-        # no NH4 left to make amm nitrate
-        mass['NH4N03'] = 0
-        # some s04 gets wasted
-        # rem_SO4 = +ve
-
-    # else... more NH4 to SO4
-    elif moles['SO4'] < (moles['NH4'] / 2):  # more NH4 than SO4 for reactions
-
-        # all of the SO4 gets used in reaction
-        mass['(NH4)2SO4'] = mass['SO4'] * 1.375  # ratio of SO4 to (NH4)2SO4
-        # rem_so4 = 0
-
-        # some NH4 remains this time!
-        # remove 2 * no of SO4 moles used from NH4 -> SO4: 2, NH4: 5; therefore rem_nh4 = 5 - (2*2)
-        rem_nh4 = moles['NH4'] - (moles['SO4'] * 2)
-
-        if moles['NO3'] > rem_nh4:  # if more NO3 to NH4 (1 mol NO3 to 1 mol NH4)
-
-            # all the NH4 gets used up
-            mass['NH4NO3'] = rem_nh4 * 4.4  # ratio of amm nitrate to remaining nh4
+            # all of the NH4 gets used up making amm sulph.
+            mass['(NH4)2SO4'][i] = mass['NH4'][i] * 7.3  # ratio of molecular weights between amm sulp and nh4
             # rem_nh4 = 0
 
-            # left over NO3
-            # rem_no3 = +ve
+            # no NH4 left to make amm nitrate
+            mass['NH4NO3'][i] = 0
+            # some s04 gets wasted
+            # rem_SO4 = +ve
 
-        elif moles['NO3'] < rem_nh4:  # more remaining NH4 than NO3
+        # else... more NH4 to SO4
+        elif moles['SO4'][i] < (moles['NH4'][i] / 2):  # more NH4 than SO4 for reactions
 
-            # all the NO3 gets used up
-            mass['NH4NO3'] = mass['NO3'] * 1.29
-            # rem_no3 = 0
+            # all of the SO4 gets used in reaction
+            mass['(NH4)2SO4'][i] = mass['SO4'][i] * 1.375  # ratio of SO4 to (NH4)2SO4
+            # rem_so4 = 0
 
-            # some left over nh4 still
-            # rem_nh4_2ndtime = +ve
+            # some NH4 remains this time!
+            # remove 2 * no of SO4 moles used from NH4 -> SO4: 2, NH4: 5; therefore rem_nh4 = 5 - (2*2)
+            rem_nh4 = moles['NH4'][i] - (moles['SO4'][i] * 2)
 
-    return moles
+            if moles['NO3'][i] > rem_nh4:  # if more NO3 to NH4 (1 mol NO3 to 1 mol NH4)
 
+                # all the NH4 gets used up
+                mass['NH4NO3'][i] = rem_nh4 * 4.4  # ratio of amm nitrate to remaining nh4
+                # rem_nh4 = 0
+
+                # left over NO3
+                # rem_no3 = +ve
+
+            elif moles['NO3'][i] < rem_nh4:  # more remaining NH4 than NO3
+
+                # all the NO3 gets used up
+                mass['NH4NO3'][i] = mass['NO3'][i] * 1.29
+                # rem_no3 = 0
+
+                # some left over nh4 still
+                # rem_nh4_2ndtime = +ve
+
+    return mass
+
+def internal_time_completion(data, date_range):
+
+    """
+    Set up new dictionary for data with a complete time series (no gaps in the middle).
+    :param: data (must be a dictionary with a list of datetimes with the keyname 'time', i.e. data['time'])
+    :return: data_full
+
+    Done by checking if time_i from the complete date range (with no time gaps) exists in the data, and if so, extract
+    out the values and put it into the new dictionary.
+    """
+
+
+    # set up temporary dictionaries for data (e.g. data_full) with empty arrays for each key, ready to be filled
+    data_full = {}
+    for h in data.iterkeys():
+        data_full[h] = np.empty(len(date_range))
+        data_full[h][:] = np.nan
+
+    # replace time with date range
+    data_full['time'] = date_range
+
+    # step through time and time match data for extraction
+    for t, time_t in enumerate(date_range):
+        idx = np.where(data['time'] == time_t)[0]
+
+        # if not empty, put in the data to new array
+        if idx != []:
+            for h in data.iterkeys():
+                data_full[h][t] = data[h][idx]
+
+    return data_full
+
+def convert_mass_to_kg_kg(mass, WXT, aer_particles):
+
+    """
+    Convert mass molecules from g m-3 to kg kg-1
+
+    :param mass
+    :param WXT (for meteorological data)
+    :param aer_particles (not all the keys in mass are the species, therefore only convert the species defined above)
+    :return: mass_kg_kg: mass in kg kg-1 air
+    """
+
+    # convert temperature to Kelvin
+    T_K = WXT['Tair'] + 273.15
+    p_Pa = WXT['press'] * 100
+
+    # density of air [kg -3] # assumes dry air atm
+    # p = rho * R * T [K]
+    dryair_rho = p_Pa / (286.9 * T_K)
+
+    # convert g m-3 air to kg kg-1 of air
+    mass_kg = {'time': mass['time']}
+    for aer_i in aer_particles:
+        mass_kg[aer_i] = mass[aer_i] * 1e-3 / dryair_rho
+
+    return mass_kg_kg
 
 def main():
 
@@ -108,56 +252,61 @@ def main():
     # data year
     year = '2016'
 
+    # aerosol particles to calculate (OC = Organic carbon, CBLK = black carbon, both already measured)
+    # match dictionary keys further down
+    aer_particles = ['(NH4)2SO4', 'NH4NO3', 'NaCl', 'OC', 'CBLK']
+
     # ==============================================================================
     # Read data
     # ==============================================================================
 
     # Read in species by mass data
-    # Units are micro grams m-3
-    massfname = 'PM_North_Kensington_'+year+'.csv'
-    massfilepath = massdatadir + massfname
-    massrawData = np.genfromtxt(massfilepath, delimiter=',', dtype="|S20") # includes the header
-
-    mass = {'time': np.array([dt.datetime.strptime(i[0], '%d/%m/%Y %H:%M') for i in massrawData[1:]])}
-
-    # get headers without the site part of it (S04@NK to S04)
-    headers = [i.split('@')[0] for i in massrawData[0][1:]]
-
-    # ignore first entry, as that is the date&time
-    for h, header_site in enumerate(massrawData[0][1:]):
-
-        # get the main part of the header from the
-        split = header_site.split('@')
-        header = split[0]
-
-        # turn '' into nans
-        # convert from micrograms to grams
-        mass[header] = np.array([np.nan if i[h+1] == '' else i[h+1] for i in massrawData[1:]], dtype=float) * 1e-06
+    # Units are grams m-3
+    mass_in = read_mass_data(massdatadir, year)
 
 
-    # QAQC - turn all negative values in each column into nans if one of them is negative
-    for header_i in headers:
-        idx = np.where(mass[header_i] < 0.0)
-        for header_j in headers:
-            mass[header_j][idx] = np.nan
-
-
-    # Read RH data
+    # Read WXT data
     wxtfilepath = wxtdatadir + wxt_inst_site + '_' + year + '_15min.nc'
-    WXT = eu.netCDF_read(wxtfilepath, vars=['RH', 'Tair','press', 'time'])
-    WXT['time'] -= dt.timedelta(minutes=15) # change time from 'obs end' to 'start of obs', same as the other datasets
+    WXT_in = eu.netCDF_read(wxtfilepath, vars=['RH', 'Tair','press', 'time'])
+    WXT_in['time'] -= dt.timedelta(minutes=15) # change time from 'obs end' to 'start of obs', same as the other datasets
 
+    # Trim times
+    # as WXT and mass data are 15 mins and both line up exactly already
+    #   therefore trim WXT to match mass time
+    mass, WXT_in = trim_mass_wxt_times(mass_in, WXT_in)
 
-    # Process the variables
+    # Time match so mass and WXT times line up INTERNALLY as well
+    date_range = eu.date_range(WXT_in['time'][0], WXT_in['time'][-1], 15, 'minutes')
 
-    # Convert into moles
+    # begin time matching
+    print 'beginning time matching for WXT...'
 
+    # make sure there are no time stamp gaps in the data so mass and WXT will match up perfectly, timewise.
+    WXT = internal_time_completion(WXT_in, date_range)
+
+    # end time matching
+    print 'end time matching for WXT...'
+
+    # begin time matching
+    print 'beginning time matching for mass...'
+
+    mass = internal_time_completion(mass_in, date_range)
+
+    # end time matching
+    print 'end time matching for mass...'
+
+    # ==============================================================================
+    # Process data
+    # ==============================================================================
+
+    # molecular mass of each molecule
     mol_mass_amm_sulp = 132
     mol_mass_amm_nit = 80
     mol_mass_nh4 = 18
     mol_mass_n03 = 62
     mol_mass_s04 = 96
 
+    # Convert into moles
     # calculate number of moles (mass [g] / molar mass)
     # 1e-06 converts from micrograms to grams.
     moles = {'SO4': mass['SO4'] / mol_mass_s04,
@@ -166,7 +315,8 @@ def main():
 
 
     # calculate ammonium sulphate and ammonium nitrate from gases
-    moles = calc_amm_sulph_and_amm_nit_from_gases(moles)
+    # adds entries to the existing dictionary
+    mass = calc_amm_sulph_and_amm_nit_from_gases(moles, mass)
 
     # convert chlorine into sea salt assuming all chlorine is sea salt, and enough sodium is present.
     #      potentially weak assumption for the chlorine bit due to chlorine depletion!
@@ -174,6 +324,12 @@ def main():
 
     # convert masses from g m-3 to kg kg-1_air for swelling.
     # use observed Tair and pressure from KSSW WXT to calculate air density
+    mass_kg_kg = convert_mass_to_kg_kg(mass, WXT, aer_particles)
+
+
+
+
+
 
 
 
