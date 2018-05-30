c81903d1885016e822215c7b58664171da2e6787
elliott <e.l.warren@pgr.reading.ac.uk>
2018-05-30 15:12:57 +0100

    init commit

---
+++ scripts/plot_aerFO_aerObs_runs.py
@@ -0,0 +1,55 @@
+"""
+Plot the aerFO runs together. aerFO runs are done in beta_m_from_mod_and_aerObs.py
+
+Created by Elliott Wed 30 May 2018
+"""
+
+import sys
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+import datetime as dt
+
+from copy import deepcopy
+import colorsys
+
+import ellUtils as eu
+
+if __name__ == '__main__':
+
+    # ---------------------------
+    # Setup
+    # ---------------------------
+
+    # directories
+    maindir = 'C:/Users/Elliott/Documents/PhD Reading/PhD Research/Aerosol Backscatter/MorningBL/'
+    datadir = 'C:/Users/Elliott/Documents/PhD Reading/PhD Research/Aerosol Backscatter/MorningBL/data/'
+    savedir = maindir + 'figures/model_eval/'
+    npydir = datadir + 'npy/model_eval/'
+
+    # ---------------------------
+    # Plotting
+    # ---------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    print 'END PROGRAM'
